812c122013f2c9c3c435cbe8f6557785ac55f6c6
Anmol659 <anmolverma1357@gmail.com>
2024-11-22 23:21:51 +0530

    Update HashMapExample.java

--- class work/HashMapExample.java
+++ class work/HashMapExample.java
@@ -7,7 +7,7 @@ import java.util.Set;
 
 public class HashMapExample {
     public static void main(String[] args) {
-        // Using raw HashMap to allow mixed key and value types
+       
         HashMap<Object, Object> map = new HashMap<>();
         map.put(1, "3");
         map.put("1", "32");
@@ -18,7 +18,7 @@ public class HashMapExample {
 
         System.out.println("Map with mixed types: " + map);
 
-        // Using HashMap with specified types (String, Integer)
+        
         HashMap<String, Integer> kvmap = new HashMap<>();
         kvmap.put("A", 45);
         kvmap.put("A", 452);
@@ -29,7 +29,7 @@ public class HashMapExample {
         System.out.println("Map size: " + kvmap.size());
         System.out.println("Contains value 45: " + kvmap.containsValue(45));
 
-        // Getting entry set for the first map
+       
         Set<Map.Entry<Object, Object>> set = map.entrySet();
         System.out.println("Entry Set: " + set);
 
@@ -39,14 +39,14 @@ public class HashMapExample {
             System.out.println(me.getKey() + ": " + me.getValue());
         }
 
-        // Displaying keys and values for kvmap
+       
         Set<String> skey = kvmap.keySet();
         System.out.println("Key Set for kvmap: " + skey);
 
         Collection<Integer> vkey = kvmap.values();
         System.out.println("Values for kvmap: " + vkey);
 
-        // Using Hashtable
+       
         Hashtable<String, Integer> htable = new Hashtable<>();
         htable.put("d", 45);
         htable.put("d1", 45);
